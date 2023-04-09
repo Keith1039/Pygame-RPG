9db594e013a25fb976fd5c9a1c8cf21914d07231
Keith Compere <keithcompere150@gmail.com>
2023-04-08 22:38:14 -0400

    Pygame-RPG 7 Upgrading Manager Classes
    Before this change, the manager classes used 'if' statements in order to
    properly control what was occuring on the screen. This however, is not
    efficient since when the scope of the project grows, so will the amount
    of 'if statements. Instead of using 'if' statements, I elected to use
    Python dicts.
    
    The dicts that each manager uses are defined within their own files which
    will make it easier to debug. I also took this time to fix some...
    questionable programming decisions I had made to acomodate the old system.
    The code in the rpg2.py file is now more streamlined.
    
    As a result of using python dicts the previous ani_array variables now store
    tuples instead of arrays. As such, they have been renamed ani_tuple and
    their `change_array` function has become `change_tuple`. The
    reason for this change is because lists cannot be returned by a dict but
    tuples can. The reason is because lists can grow in size while tuples
    are more like standard arrays.
    
    Another change is that the context variable in some of the managers has
    been removed and now only ScreenManager has a context variable. This
    change is to make it easier to centralize everything into a single
    omni-manager made from ScreenManager in the later stages of this project.
    
    Some tests have needed to be tweaked due to the changes and I've
    confirmed that the changes work without issue for now.

--- managers/Object_Animation_Manager.py
+++ managers/Object_Animation_Manager.py
@@ -1,18 +1,21 @@
-chestAniR = 4, "Item_Art&Object_Art/Usable/Chest_Opening_R "
-chestAniL = 4, "Item_Art&Object_Art/Usable/Chest_Opening_L "
+chestAniR = (4, "Item_Art&Object_Art/Usable/Chest_Opening_R ")
+chestAniL = (4, "Item_Art&Object_Art/Usable/Chest_Opening_L ")
 
+object_ani_dict = {"Chest": chestAniL}
 #Could be useful
 
 class ObjectAnimationManager:
     def __init__(self):
-        self.aniArray = []
+        self.aniTuple = ()
 
-    def set_array(self, context):
+    def set_tuple(self, context):
         if context == "Background1":
-            self.aniArray = chestAniL
-    def change_array(self, knight, pos, interactable):
-        if pos > interactable[0][0] and pos < interactable[0][1] and interactable[1] == "Chest" and interactable[
-            2] == False:
-            self.aniArray = chestAniL
+            self.aniTuple = chestAniL
+
+    def change_tuple(self, knight, pos, interactable):
+        within_range = pos > interactable[0][0] and pos < interactable[0][1]
+        print(within_range)
+        if within_range and not interactable[2]:
+            self.aniTuple = object_ani_dict.get(interactable[1])
             knight.status = "Opening Chest"
             interactable[2] = True
