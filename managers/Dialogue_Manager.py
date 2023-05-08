433146e77ef9dfa00a85a49cd9655a7b5a510b1b
Keith Compere <keithcompere150@gmail.com>
2023-05-08 11:34:39 -0400

    Pygame-RPG 8 Creating and Testing Dialogue Manager
    parent dca05a2a869a1ffc3cea8ff65af868828467d7b3
    author Keith Compere <keithcompere150@gmail.com> 1683560079 -0400
    committer Keith Compere <keithcompere150@gmail.com> 1684802583 -0400
    
    Dialogue Manager is the class that is responsible for managing the
    dialogue in the game. The Dialogue Manager class has the logic for how
    dialogue flows in the game. The `draw_dialogue()` function actually
    draws the dialogue on screen for the player. Currently this is in the
    form of a set timer, in the future, I would want this to be in the form
    of the player pressing the 'Enter' key but I'm having trouble with that
    implementation.
    
    Portraits for the characters are found via a python dict and the input
    for this dict is parsed from text found in a file. If a portrait is
    found then `draw_dialogue()` will draw a character box and the portrait
    inside the character box. The trigger for a text dialogue is through an
    Event object through the eventType parameter. In the future, all player
    interactions might be handled by a 'handle_interactions()' function for
    the knight object which will combine all the logic for the interactions
    into one place.
    
    The textEnable variable, determines whether `draw_dialogue()` can be
    used. textEnable is set to true when a dialogue Event object is
    interacted with and is set to False when the dialogue event is over,
    flipping the Event's activated variable to True in the process.
    
    All the portraits and the pictures used for dialogue manager is stored
    in the portraits directory. This change also includes the addition of a
    brief description of the Dialogue Manager class.

---
+++ managers/Dialogue_Manager.py
@@ -0,0 +1,55 @@
+import os
+import pygame as game
+
+# Dict that will point to the files where the dialogue still is
+text_dict = {}
+# Portrait dict takes in a name and then gets the portrait
+portrait_dict = {"Knight": game.image.load("portraits/Knight.png")  }
+class DialogueManager: 
+    def __init__(self ):
+        self.file = None
+        self.portrait = None 
+        self.name = ""
+        self.characterBox = game.image.load("portraits/character_box.png")
+        self.textBox = game.image.load("portraits/text_box.png")
+        self.event = None
+        self.prevText = ""
+
+    def load_file(self, event):
+        # Loads the file that we read from
+        self.event = event
+        self.file = open(event.path)
+    
+    # Returns a single text line. Probably going to have to enforce a char limit too huh...
+    def get_text(self):
+        text = self.file.readline()
+        self.prevText = text
+        #Clears file's value if EOF has been reached.
+        if text == "":
+            self.clear_file()
+            self.prevText = "1"
+        else:
+            text = self.load_portrait(text)
+        return text
+    
+    def clear_file(self):
+        self.file.close()
+        self.event.activated = True
+        self.event = None
+        self.file = None
+
+    def load_portrait(self, text):
+        # Finding the character name
+        name = ""
+        i = 0
+        for i in range(len(text)):
+            if text[i] == ":":
+                text = text[i+1: len(text)]
+                break
+            else:
+                name += text[i]
+        self.name = name
+        self.prevText = text
+        self.portrait = portrait_dict.get(name) # This can be None btw so there has to be logic checking this
+        # Returns text that has name stripped from it
+        return text
---
+++ managers/Dialogue_Manager_test.py
@@ -0,0 +1,33 @@
+from managers.Dialogue_Manager import DialogueManager
+from managers.Screen_Manager import Event
+#import pygame as game
+
+dialogueManager = DialogueManager()
+mockDialogEvent = Event(None, "Dialogue","event_text/Test_dialogue.txt")
+
+def mock_draw_dialogue(dialogueTimer):
+    # Version of draw_dialogue that doesn't do any of the drawing.
+    text = dialogueManager.prevText
+    if dialogueTimer % 180 == 0:
+        text = dialogueManager.get_text()
+    #There is an easier way to do this but it makes my eyes bleed so no :)
+    return text != ""
+def test_load_file():
+    # Load file just uses open() to get a file object. Loading a valid file should 
+    # actually load the file thus making file not None
+    dialogueManager.load_file(mockDialogEvent)
+    assert dialogueManager.file != None
+
+def test_clear_file():
+    # Checks if the file value in the manager is None and the file is cleared properly
+    # Also checks if the events activated value has been changed. This logic is going to cause problems in the future
+    # But that is future Keith's problem.
+    dialogueManager.load_file(mockDialogEvent)
+    dialogueTimer = 180
+    flag = True
+    file = dialogueManager.file
+    while flag:
+        flag = mock_draw_dialogue(dialogueTimer)
+        dialogueTimer += 1
+    # Might make dialogueManager keep the file even after it has been closed.
+    assert file.closed and dialogueManager.file == None and mockDialogEvent.activated
