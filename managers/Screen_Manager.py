e7fb9b2193456673f9580a4e7c505841f20df60c
Keith Compere <keithcompere150@gmail.com>
2023-07-27 12:39:53 -0400

    Pygame-RPG 9.2 Housekeeping and bugfixing
    It was discovered that the states of the events were saved by
    SaveManager. This led to a glitch where you could open a chest, load a
    previous save from before you opened the chest and be unable to open
    said chest. As such, SaveManager needed to be changed to include this
    vital feature. Tests for this were also written.
    
    Other changes:
    - created the `load()` function to simplify loading JSON data into the
      object
    - created the `load_data()` function to simplify loading information in
      SaveManager
    - formatting changes

--- managers/Screen_Manager.py
+++ managers/Screen_Manager.py
@@ -9,6 +9,15 @@ class Event:
         self.activated = False
         self.path = path
 
+    def load(self, infoDict):
+        self.range = tuple(infoDict["range"])
+        self.eventType = infoDict["eventType"]
+        self.activated = infoDict["activated"]
+        self.path = infoDict["path"]
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
 # Screen manager should draw the actual stuff I guess?
 # For now there's only going to be two options moving to the left screen and moving to the right screen
 # Moving to right screen = 1
@@ -17,8 +26,9 @@ chestB1 = Event((500, 600), "Chest")
 screen_dict = {("Background1", 1): "Background2", ("Background2", -1): "Background1"}
 background_dict ={"Background1": "Background_Art/gothic_chapel_portfolio_1422x800.png",
                   "Background2": "Background_Art/PNG/game_background_1/game_background_1.png"}
-interactables_dict = {"Background1": (chestB1, ), "Background2": ()}
-objects_dict = {"Background1": ((800, 500), True), "Background2": ()} # NEEDS TO BE SAVED
+interactables_dict = {"Background1": (chestB1, ), "Background2": ()}   # NEEDS TO BE SAVED
+objects_dict = {"Background1": ((800, 500), True), "Background2": ()}  # NEEDS TO BE SAVED
+
 class ScreenManager:
     def __init__(self, screen):
         self.interactables = ()
@@ -27,7 +37,9 @@ class ScreenManager:
         self.objects = ()
         self.objectAni = ObjectAnimationManager()
         self.objectDict = objects_dict
+        self.interactablesDict = interactables_dict
         self.apply_context()
+
 
     def change_screen(self, pos):
         mover = 0
@@ -57,3 +69,4 @@ class ScreenManager:
         self.context = context
         self.screen = game.image.load(background_dict.get(self.context))  # Draws the screen based on the new context
         self.apply_context()  # Applies the current context to reload the other things on screen
+
